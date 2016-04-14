ecee6bbb3598b9db6d362a4d554843565c05c143
Douglas Bates <dmbates@gmail.com>
2016-04-14 17:37:10 -0500

    Update example files to version 1

--- src/Feather.jl
+++ src/Feather.jl
@@ -1,12 +1,14 @@
 module Feather
 
-using CxxWrap, DataArrays, DataFrames
+using FlatBuffers
+#using CxxWrap, DataArrays, DataFrames
 
-wrap_module(Libdl.find_library(["libfeatherjl.so"], [Pkg.dir("Feather", "deps", "usr", "lib")]))
+#wrap_module(Libdl.find_library(["libfeatherjl.so"], [Pkg.dir("Feather", "deps", "usr", "lib")]))
 
 export
     Reader
 
+include("tables.jl")
 include("reader.jl")
 
 end # module
--- src/reader.jl
+++ src/reader.jl
@@ -1,51 +1,28 @@
+const magic = "FEA1"
+
 type Reader
     path::AbstractString
-    nrow::Int64
-    ncol::Int64
-    columns::Vector{CxxWrap.UniquePtr{Column}}
-    names::Vector{UTF8String}
-    ptr
+    io::IO
+    metadata::CTable
 end
 
 function Reader(path::AbstractString)
-    uptr = openFeatherTable(path)
-    ptr = get(uptr)
-    cols = [getcolumn(ptr, i - 1) for i in 1:num_columns(ptr)]
-    Reader(path, num_rows(ptr), length(cols), cols, map(c -> utf8(name(get(c))), cols), uptr)
+    io = IOBuffer(Mmap.mmap(path))
+    pos = position(skip(seekend(io), - 8)) # last 8 bytes are [..., UInt32(metadata size), magic]
+    pos -= read(io, Int32)                 # start of the metadata
+                                           # file should begin and end with "FEA1"
+    if io.data[1:4] ≠ magic.data || readbytes(io) ≠ magic.data
+        throw(ArgumentError(string("File: ", path, " is not in feather format")))
+    end
+    pos += read(seek(io, pos), Int32)      # flatbuffer offset to root table at beginning of buffer
+    Reader(path, io, CTable(io, pos))
 end
-
-Base.size(r::Reader) = (r.nrow, r.ncol)
-
-Base.size(r::Reader, i::Integer) = i == 1 ? r.nrow : (i == 2 ? r.ncol : 1)
-
-const jtypes = [Bool, Int8, Int16, Int32, Int64, UInt8, UInt16, UInt32, UInt64,
-     Float32, Float64, UTF8String, Ptr{Void}]
-
-jtype(upt::CxxWrap.UniquePtr{Column}) = jtypes[datatype(get(upt)) + 1]
-
-@enum Column_Type PRIMITIVE CATEGORY TIMESTAMP DATE TIME
-
-coltype(upt::CxxWrap.UniquePtr{Column}) = Column_Type(columntype(get(upt)))
 
 function Base.show(io::IO, r::Reader)
-    println(io, string('[', r.nrow, " × ", r.ncol, "] @ ", r.path))
-    mxnm = maximum(map(length, r.names)) + 2
-    for i in eachindex(r.names)
-        coli = r.columns[i]
-        println(io, " ", rpad(r.names[i], mxnm), ": ", coltype(coli), "(", jtype(coli), ")")
-    end
-end
-
-if false
-
-function Base.Dict(r::Reader)
-    nms, cols, res = r.names, r.columns, Dict{Symbol, Any}()
-    for i in eachindex(nms)
-        res[symbol(nms[i])] = values(cols[i])
-    end
-    res
-end
-
-DataFrames.DataFrame(r::Reader) = DataFrame(Dict(r))
-
+    println(io, string('[', r.metadata[:numRows], " × ", length(r.metadata[:columns]), "] @ ", r.path))
+#    mxnm = maximum(map(length, r.names)) + 2
+#    for i in eachindex(r.names)
+#        coli = r.columns[i]
+#        println(io, " ", rpad(r.names[i], mxnm), ": ", coltype(coli), "(", jtype(coli), ")")
+#    end
 end

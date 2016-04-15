691da2d26575c56733dfd3a2f3474a029e82b1c2
Douglas Bates <dmbates@gmail.com>
2016-04-15 16:54:58 -0500

    Switch to new FlatBuffers format

--- src/Feather.jl
+++ src/Feather.jl
@@ -1,10 +1,5 @@
 module Feather
 
-using FlatBuffers
-#using CxxWrap, DataArrays, DataFrames
-
-#wrap_module(Libdl.find_library(["libfeatherjl.so"], [Pkg.dir("Feather", "deps", "usr", "lib")]))
-
 export
     Reader
 
--- src/tables.jl
+++ src/tables.jl
@@ -1,61 +1,63 @@
 using FlatBuffers
 
-@enum(TimeUnit, Second, Millisecond, Nanosecond)
+@enum(Type_, BOOL = 0, INT8 = 1, INT16 = 2, INT32 = 3, INT64 = 4,
+  UINT8 = 5, UINT16 = 6, UINT32 = 7, UINT64 = 8,
+  FLOAT = 9, DOUBLE = 10,  UTF8 = 11,  BINARY = 12,
+  CATEGORY = 13, TIMESTAMP = 14, DATE = 15, TIME = 16)
 
-type PrimitiveArray <: FlatBuffers.Table
-    io::IO
-    pos::Integer
-    members::Dict{Symbol, Tuple{Int16, DataType, Any}}
-    vtbloff::Int32
-    vtblsz::Int16
+@enum(Encoding_, PLAIN = 0, DICTIONARY = 1)
+
+@enum(TimeUnit_, SECOND = 0, MILLISECOND = 1, NANOSECOND = 2)
+
+for nm in [:PrimitiveArray, :CategoryMetadata, :TimestampMetadata, :DateMetadata, :TimeMetadata, :Column, :CTable]
+    @eval begin
+        type $nm <: FlatBuffers.Table
+            io::FlatBuffers.TableIO
+            memb::FlatBuffers.Membrs
+        end
+
+        $nm(io::IO, pos) = $nm(FlatBuffers.TableIO(io, pos), $(symbol(string(nm,"_members"))))
+    end
 end
 
-function PrimitiveArray(io::IO, pos::Integer)
-    vtbloff = read(seek(io, pos), Int32)
-    vtblsz = read(seek(io, pos - vtbloff), Int16)
-    PrimitiveArray(io, pos, Dict{Symbol, Tuple{Int16, DataType, Any}}(
-        :type => (Int16(4), UInt8, zero(UInt8)),
-        :encoding => (Int16(6), UInt8, zero(UInt8)),
-        :offset => (Int16(8), Int64, zero(Int64)),
-        :length => (Int16(10), Int64, zero(Int64)),
-        :null_count => (Int16(12), Int64, zero(Int64)),
-        :total_bytes => (Int16(14), Int64, zero(Int64))
-    ), vtbloff, vtblsz)
-end
+typealias TypeMetadata Union{CategoryMetadata, TimestampMetadata, DateMetadata, TimeMetadata}
 
-type Column <: FlatBuffers.Table
-    io::IO
-    pos::Integer
-    members::Dict{Symbol, Tuple{Int16, DataType, Any}}
-    vtbloff::Int32
-    vtblsz::Int16
-end
+const PrimitiveArray_members = FlatBuffers.Membrs(
+    :type => (4, UInt8, 0),
+    :encoding => (6, UInt8, 0),
+    :offset => (8, Int64, 0),
+    :length => (10, Int64, 0),
+    :null_count => (12, Int64, 0),
+    :total_bytes => (14, Int64, 0)
+)
 
-function Column(io::IO, pos::Integer)
-    vtbloff = read(seek(io, pos), Int32)
-    vtblsz = read(seek(io, pos - vtbloff), Int16)
-    Column(io, pos, Dict{Symbol, Tuple{Int16, DataType, Any}}(
-        :name => (Int16(4), UTF8String, utf8("")),
-        :values => (Int16(6), PrimitiveArray, PrimitiveArray(IOBuffer([0x00,0x00,0x00,0x00]), 0))
-    ), vtbloff, vtblsz)
-end
+const CategoryMetadata_members = FlatBuffers.Membrs(
+    :levels => (4, PrimitiveArray, PrimitiveArray(IOBuffer("\0\0\0\0"), 0)),
+    :ordered => (6, Bool, false)
+)
 
-type CTable <: FlatBuffers.Table
-    io::IO
-    pos::Integer
-    members::Dict{Symbol, Tuple{Int16, DataType, Any}}
-    vtbloff::Int32
-    vtblsz::Int16
-end
+const TimestampMetadata_members = FlatBuffers.Membrs(
+    :unit => (4, UInt8, 0),
+    :timezone => (6, UTF8String, "")
+)
 
-function CTable(io::IO, pos::Integer)
-    vtbloff = read(seek(io, pos), Int32)
-    vtblsz = read(seek(io, pos - vtbloff), Int16)
-    CTable(io, pos, Dict{Symbol, Tuple{Int16, DataType, Any}}(
-        :description => (Int16(4), UTF8String, utf8("")),
-        :numRows => (Int16(6), Int64, zero(Int64)),
-        :columns => (Int16(8), Vector{Column}, Column[]),
-        :version => (Int16(10), Int32, zero(Int32)),
-        :metadata => (Int16(12), UTF8String, utf8(""))
-    ), vtbloff, vtblsz)
-end
+const DateMetadata_members = FlatBuffers.Membrs()
+
+const TimeMetadata_members = FlatBuffers.Membrs(
+    :unit => (4, UInt8, 0)
+)
+
+const Column_members = FlatBuffers.Membrs(
+    :name => (4, UTF8String, ""),
+    :values => (6, PrimitiveArray, PrimitiveArray(IOBuffer("\0\0\0\0"), 0)),
+    :metadata => (8, TypeMetadata, nothing),
+    :user_metadata => (10, UTF8String, "")
+)
+
+const CTable_members = FlatBuffers.Membrs(
+    :description => (Int16(4), UTF8String, ""),
+    :numRows => (Int16(6), Int64, 0),
+    :columns => (Int16(8), Vector{Column}, Column[]),
+    :version => (Int16(10), Int32, 0),
+    :metadata => (Int16(12), UTF8String, ""),
+)

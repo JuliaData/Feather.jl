0cf757a9561c07c76e1a1e658c0218a3495c4b90
Douglas Bates <dmbates@gmail.com>
2016-04-22 14:39:08 -0500

    Add tests.

--- src/Feather.jl
+++ src/Feather.jl
@@ -6,29 +6,34 @@ import DataFrames: names, ncol, nrow
 addHeaderDir(Pkg.dir("Feather", "deps", "usr", "include"))
 cxxinclude("feather/metadata_generated.h")
 
-export TableReader,
+export
     names,
     ncol,
     nrow
 
+const magic = "FEA1"
 
-type TableReader
+type Reader
     ptr::Cxx.CppPtr
     path::AbstractString
     buf::IO
 end
 
-function TableReader(path::AbstractString)
-    buf = IOBuffer(Mmap.mmap(path))
-    metasz = read(skip(seekend(buf), -8), Int32)
-    mpt = pointer(buf.data) + position(skip(buf, -(metasz + sizeof(Int32))))
-    ptr = icxx"feather::fbs::GetCTable($mpt);"
-    TableReader(ptr, path, buf)
+function Reader(path::AbstractString)
+    io = IOBuffer(Mmap.mmap(path))
+    pos = position(skip(seekend(io), -8)) # last 8 bytes are [..., UInt32(metadata size), magic]
+    pos -= read(io, Int32)                # start of the metadata
+                                          # file should begin and end with "FEA1"
+    if io.data[1:4] ≠ magic.data || read(io) ≠ magic.data
+        throw(ArgumentError(string("File: ", path, " is not in feather format")))
+    end
+    ptr = icxx"feather::fbs::GetCTable($(pointer(io.data) + pos));"
+    Reader(ptr, path, io)
 end
 
-nrow(tbl::TableReader) = icxx"$(tbl.ptr)->num_rows();"
-ncol(tbl::TableReader) = Int(icxx"$(tbl.ptr)->columns()->size();")
-name(tbl::TableReader, i::Integer) = pointer_to_string(icxx"$(tbl.ptr)->columns()->Get($i - 1)->name()->c_str();")
-names(tbl::TableReader) = [name(tbl, i) for i in 1:ncol(tbl)]
+nrow(tbl::Reader) = icxx"$(tbl.ptr)->num_rows();"
+ncol(tbl::Reader) = Int(icxx"$(tbl.ptr)->columns()->size();")
+name(tbl::Reader, i::Integer) = pointer_to_string(icxx"$(tbl.ptr)->columns()->Get($i - 1)->name()->c_str();")
+names(tbl::Reader) = [name(tbl, i) for i in 1:ncol(tbl)]
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,8 @@ using Feather
 using Base.Test
 
 # write your own tests here
-rdr = TableReader(Pkg.dir("Feather", "test", "data", "mtcars.feather"))
+testdir = dirname(@__FILE__)
+mtcars = Feather.Reader(joinpath(testdir, "data", "mtcars.feather"))
+@test nrow(mtcars) == 32
+@test ncol(mtcars) == 11
+@test names(mtcars) == ["mpg","cyl","disp","hp","drat","wt","qsec","vs","am","gear","carb"]

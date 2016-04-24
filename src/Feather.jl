d00ecbae0a57d22f6cd3e274d366dde8e7210218
Andreas Noack <andreasnoackjensen@gmail.com>
2016-04-23 23:19:09 -0400

    Define Column type.

--- src/Feather.jl
+++ src/Feather.jl
@@ -36,4 +36,91 @@ ncol(tbl::Reader) = Int(icxx"$(tbl.ptr)->columns()->size();")
 name(tbl::Reader, i::Integer) = pointer_to_string(icxx"$(tbl.ptr)->columns()->Get($i - 1)->name()->c_str();")
 names(tbl::Reader) = [name(tbl, i) for i in 1:ncol(tbl)]
 
+type Column{T} <: DenseVector{T}
+    r::Reader
+    colnr::Int
+    ptr::Ptr{T}   # keep a pointer to the data to vaoid reloading the pointer
+    bitmask::Nullable{BitArray{1}}
+end
+
+_offset(tbl::Reader, i::Integer) = icxx"$(tbl.ptr)->columns()->Get($i - 1)->values()->offset();"
+_null_count(tbl::Reader, i::Integer) = icxx"$(tbl.ptr)->columns()->Get($i - 1)->values()->null_count();"
+function _type(tbl::Reader, i::Integer)
+    t = icxx"$(tbl.ptr)->columns()->Get($i - 1)->values()->type();"
+    if t == icxx"feather::fbs::Type_BOOL;"
+        return Bool
+    elseif t == icxx"feather::fbs::Type_INT8;"
+        return Int8
+    elseif t == icxx"feather::fbs::Type_INT16;"
+        return Int16
+    elseif t == icxx"feather::fbs::Type_INT32;"
+        return Int32
+    elseif t == icxx"feather::fbs::Type_INT64;"
+        return Int64
+    elseif t == icxx"feather::fbs::Type_UINT8;"
+        return UInt8
+    elseif t == icxx"feather::fbs::Type_UINT16;"
+        return UInt16
+    elseif t == icxx"feather::fbs::Type_UINT32;"
+        return UInt32
+    elseif t == icxx"feather::fbs::Type_UINT64;"
+        return UInt64
+    elseif t == icxx"feather::fbs::Type_FLOAT;"
+        return Float32
+    elseif t == icxx"feather::fbs::Type_DOUBLE;"
+        return Float64
+    elseif t == icxx"feather::fbs::Type_UTF8;"
+        return UTF8String
+    else
+        error("type not handled yet!")
+    end
+end
+
+function Column(r::Reader, i::Integer)
+    if !(1 <= i <= ncol(r))
+        error("illegal column index")
+    end
+
+    # fetch element type for column
+    T = _type(r, i)
+
+    # get pointer to beginning of data
+    ptr = pointer(r.buf.data) + _offset(r, i)
+
+    # check if there are missing values
+    if _null_count(r, i) > 0
+        # missing values are marked with bitmask which is stored first in steps of 32 bit
+        n = nrow(r)
+        ba = BitArray{1}(n)
+        nullbytes = 1
+        while nullbytes << 3 < n
+            nullbytes += 1
+        end
+        Base.unsafe_copy!(convert(Ptr{UInt8}, pointer(ba.chunks)), ptr, nullbytes)
+        bitmask = Nullable(ba)
+    else
+        bitmask = Nullable{BitArray{1}}()
+    end
+
+    return Column{T}(r, i, convert(Ptr{T}, ptr + nullbytes), bitmask)
+end
+
+Base.size(c::Column) = (nrow(c.r),)
+
+function Base.getindex(c::Column, i::Integer)
+    x = unsafe_load(c.ptr, i)
+    if isnull(c.bitmask)
+        return x
+    else
+        if get(c.bitmask)[i]
+            return x
+        else
+            throw(UndefRefError())
+        end
+    end
+end
+
+Base.summary(c::Column) =
+    string(Base.dims2string(size(c)), " ", typeof(c), "\n$(name(c.r, c.colnr))")
+
 end

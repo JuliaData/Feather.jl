ef2d834fda0f82ad13f6ed3668e3ae9411af7e54
Shashi Gowda <shashi@juliacomputing.com>
2017-09-13 14:55:45 +0530

    fix WeakrRefString usage

--- src/Feather.jl
+++ src/Feather.jl
@@ -190,7 +190,7 @@ function Data.streamfrom(source::Source, ::Type{Data.Column}, ::Type{NullableVec
     offset = source.ctable.columns[col].values.offset +
              (source.ctable.columns[col].values.null_count > 0 ? Feather.getoutputlength(source.ctable.version, Feather.bytes_for_bits(source.ctable.num_rows)) : 0) +
              getoutputlength(source.ctable.version, sizeof(offsets))
-    A = [WeakRefString(pointer(source.data, offset + offsets[i]+1), Int(offsets[i+1] - offsets[i]), Int(offset + offsets[i]+1)) for i = 1:source.ctable.num_rows]
+             A = [WeakRefString(pointer(source.data)+offset+offset[1], Int(offsets[i+1] - offsets[i])) for i = 1:source.ctable.num_rows]
     bools = getbools(source, col)
     return NullableArray{WeakRefString{UInt8},1}(A, bools, source.data)
 end
